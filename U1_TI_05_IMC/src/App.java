e27e3fd9eee5e331b02435e5a0bacc5d572ffc60
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-05-30 09:14:28 -0600

    en el archivo main, se le pide al usuario que ingrese los datos necesarios para calcular el imc, para depsues mostrarlos mediante consola

--- U1_TI_05_IMC/src/App.java
+++ U1_TI_05_IMC/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner sc = new Scanner(System.in);
+
+        Persona persona = new Persona();
+
+        System.out.print("Ingrese su nombre: ");
+        persona.setNombre(sc.nextLine());
+
+        System.out.print("Ingrese su altura en metros: ");
+        persona.setAltura(sc.nextDouble());
+
+        System.out.print("Ingrese su peso en kilogramos: ");
+        persona.setPeso(sc.nextDouble());
+
+        double imc = persona.calcularIMC();
+        String clasificacion = persona.clasificarIMC();
+
+        System.out.println("Hola " + persona.getNombre());
+        System.out.printf("Tu IMC es: %.2f%n", imc);
+        System.out.println("Tu peso es: " + persona.getPeso() + " kg");
+        System.out.println("Tu altura es: " + persona.getAltura() + " m");
+        System.out.println("Clasificación: " + clasificacion);  
+
+        sc.close();
+
     }
 }
